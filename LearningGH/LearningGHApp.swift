5cc909bc59a3ae4cbf2110b5b67887543d326291
Ian Bateman <ian.bateman@icloud.com>
2022-07-14 22:03:02 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ian Bateman on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
